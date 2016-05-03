02918a98784eae1b918dd1eb510c6faeb7248361
Emma WS <ewolfrum@umich.edu>
2016-05-03 14:51:11 -0400

    Add subsetting of variables

--- read_gm_data.R
+++ read_gm_data.R
@@ -1,6 +1,9 @@
-≈gdp<- read.csv("data/data-1952.csv")
+gdp<- read.csv("data/data-1952.csv")
 
 gdp$gdp <- gdp$gdpPercap * gdp$pop
+
+keep <- c('country', 'year', 'lifeExp', 'gdp')
+gdp <- gdp[keep]
 
 write.csv(gdp, "gdpc.csv") 
 
